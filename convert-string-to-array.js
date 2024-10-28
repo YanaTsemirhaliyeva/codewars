d761f5470b9d6b30f1d387841db460f45b7db640
Yana Temirgalieva <kolesiko.yana@gmail.com>
2024-10-28 09:05:25 +0300

    task-08

---
+++ convert-string-to-array.js
@@ -0,0 +1,10 @@
+// TASK:
+// Write a function to split a string and convert it into an array of words.
+
+// Examples (Input ==> Output):
+// "Robin Singh" ==> ["Robin", "Singh"]
+
+// SOLUTION:
+function stringToArray(string) {
+    return string.split(' ');
+}
